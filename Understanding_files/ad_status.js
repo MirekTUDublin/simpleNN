f1fe4a0fe2fe70a24b9643c267036d2871213552
MirekTUDublin <123872527+MirekTUDublin@users.noreply.github.com>
2023-03-07 22:38:09 +0000

    First upload

---
+++ Understanding_files/ad_status.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+window.google_ad_status = 1;
+
+
+}
+/*
+     FILE ARCHIVED ON 22:19:24 Dec 29, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:19:23 Mar 07, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.097
+  exclusion.robots.policy: 0.085
+  cdx.remote: 0.104
+  esindex: 0.009
+  LoadShardBlock: 153.936 (6)
+  PetaboxLoader3.datanode: 134.25 (8)
+  load_resource: 1713.007 (2)
+  PetaboxLoader3.resolve: 1634.263 (2)
+*/
